b84d9e618336065aa31f14ab42a03202442b6dfc
Crisam De Gracia <crisamdegracia@gmail.com>
2019-09-04 00:59:50 +0800

    getting so worked up sa parent and children link relation uhmegaddd---  GRINDING!

--- wp-content/themes/fictional-university/page.php
+++ wp-content/themes/fictional-university/page.php
@@ -10,22 +10,56 @@ get_header(); ?>
             <p>Learn how the school of your dreams got started.</p>
         </div>
     </div>  
-</div>
+</div> 
 
 <div class="container container--narrow page-section">
 
+    <!-- $theParent is the ID of the parent page -->
+    <?php $theParent =  wp_get_post_parent_id( get_the_ID() );
+                           if( $theParent ) { ?>
+
     <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+        <p><a class="metabox__blog-home-link" href="<?php echo get_permalink( $theParent ); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title( $theParent ); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
     </div>
+
+
+
+    <?php } ?>
+
+
+    <?php
+                           //if the page has current children will return a list of children pages
+                           //false - return nothing
+                           $checkArray = get_pages(array(
+                               'child_of' => get_the_ID()
+                           )); 
+                           if($theParent or $checkArray ){ ?>
 
     <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent) ?>"><?php echo get_the_title($theParent) ?></a></h2>
         <ul class="min-list">
-            <li class="current_page_item"><a href="#">Our History</a></li>
-            <li><a href="#">Our Goals</a></li>
+
+            <?php  if( $theParent ) {
+                               $theChildOf = $theParent;
+                           } else {
+                               $theChildOf = get_the_ID();
+                           }
+                                                          wp_list_pages( array(
+
+                                                              //will remove the head title PAGES
+                                                              'title_li' => NULL,
+                                                              //will output the child of current page if has parent
+                                                              'child_of' => $theChildOf,
+                                                              
+                                                              // we can decide what is the mauuna sa list pf page
+                                                              //by going to the child dashboard page tapos
+                                                              // we can pick the number order there. gets mo?
+                                                              'sort_column' => 'menu_order'
+                                                          )) ;
+            ?>
         </ul>
     </div>
-
+    <?php } ?>
     <div class="generic-content">
         <p> <?php the_content() ?> </p>
     </div>
